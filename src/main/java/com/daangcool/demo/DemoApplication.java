2c96d8ed8ac9665cb89ac52d13308c4759a74eb4
sanghyoukjin <chingoo2@naver.com>
2021-05-03 14:21:01 +0900

    - boot pid 작업

--- src/main/java/com/daangcool/demo/DemoApplication.java
+++ src/main/java/com/daangcool/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.daangcool.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+
+        SpringApplication application = new SpringApplication(DemoApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
